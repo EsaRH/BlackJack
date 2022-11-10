2e517bd1d1a41bb9d86ef21f48d3cb7fc4ecd809
Esau RH <esauramoshdz@gmail.com>
2022-11-10 17:50:57 -0500

    Ya es funcional

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
-let firstCard = 6;
-let secondCard = 9;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+    name: "Esau",
+    chips: 200
+}
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
+let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
+}
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+    cardsEl.textContent = "Cards: ";
+    for (let hand = 0; hand < cards.length; hand++) {
+        cardsEl.textContent += cards[hand] + " ";
+    }
+
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
+
     } else if (sum === 21) {
         message = "Wohoo! You've got BlackJack";
+
         hasBlackJack = true;
     } else {
         message = "You're out of the game!";
+
         isAlive = false;
     }
     messageEl.textContent = message;
 }
+
+
 function newCard() {
-    let cards = 6;
-    sum += cards;
-    startGame();
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
+
 }
